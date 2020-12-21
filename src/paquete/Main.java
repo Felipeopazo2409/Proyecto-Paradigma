9301a35dc0414d80ddf19a53f6b0e26763c63d42
Felipe Opazo <felipeopazo2409@gmail.com>
2020-12-20 21:35:06 -0300

    version 1.1

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,10 +1,20 @@
 package paquete;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		JFrame ventana = new JFrame();
+		Home inicio = new Home();
+		Panel panel = new Panel();
+		ventana.setSize(780,500);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setResizable(true);
+		ventana.add(inicio);
 
+		
+		ventana.setVisible(true);
 	}
 
 }
---
+++ src/paquete/Panel.java
@@ -0,0 +1,17 @@
+package paquete;
+
+import java.awt.Color;
+
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+	private JLabel icono;
+	public Panel() {
+		setLayout(null);
+		setBackground(Color.BLACK);
+		
+	}
+	
+}
